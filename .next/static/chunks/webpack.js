c9ecd547b1fa2cb7d41bc4ac6d1ca754b00f154b
Ryan Massey <rmassey.95@gmail.com>
2023-02-14 13:31:56 -0500

    Create Navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e709e3770691785b"; }
+/******/ 		__webpack_require__.h = function() { return "0c0e6be98c165443"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,45 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const subfoldMenu = () => {
+    document.querySelector("#subfold-menu-opts").classList.toggle("hide");
+  };
+
+  return (
+    <>
+      <nav className="flex py-1 shadow-sm justify-around space-x-4">
+        <a
+          href="/"
+          className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
+        >
+          Home
+        </a>
+        <button
+          onClick={subfoldMenu}
+          className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900 relative"
+        >
+          Subfolders
+          <div
+            id="subfold-menu-opts"
+            className="absolute bg-white left-1/2 -translate-x-1/2 mt-6 border rounded-lg p-4 opacity-100 opacity-0 hide transition-opacity duration-300"
+          >
+            <div className="flex flex-col space-y-2">
+              <a
+                href="/programming"
+                className="rounded-lg px-2 py-1 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
+              >
+                Programming
+              </a>{" "}
+              <a
+                href="/hardware"
+                className="rounded-lg px-2 py-1 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
+              >
+                Hardware
+              </a>
+            </div>
+          </div>
+        </button>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
